4afa6cd03c7f7f0ee125a0adff44b1b8f2b9e057
Kassia Albuquerque <kassiademaria@gmail.com>
2024-08-27 17:18:04 -0300

    Configurações iniciais e as dadas em aula

---
+++ src/scripts/components.js
@@ -0,0 +1,7 @@
+import HeaderElement from "../components/Header/header.lit.js";
+import SearchElement from "../components/Search/search.lit.js";
+import FilterElement from "../components/Filter/Filter.lit.js";
+
+customElements.define('eve-header', HeaderElement);
+customElements.define('eve-search', SearchElement);
+customElements.define('eve-filter', FilterElement);
